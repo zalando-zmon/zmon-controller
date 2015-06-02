af1e3b084efaf5fb6526c34c0c418b2f38b2a21e
Jan Mussler <jan.mussler@zalando.de>
2015-06-02 15:37:31 +0200

    Change +x flag

--- create_squashed.sh
+++ create_squashed.sh
@@ -1,5 +1,7 @@
 
 export IMAGE=zmon-controller
+
 docker build -t os-registry.stups.zalan.do/stups/$IMAGE:$1-unsquashed .
 docker save os-registry.stups.zalan.do/stups/$IMAGE:$1-unsquashed | docker-squash -verbose -t os-registry.stups.zalan.do/stups/$IMAGE:$1 | docker load
 docker push os-registry.stups.zalan.do/stups/$IMAGE:$1
+
