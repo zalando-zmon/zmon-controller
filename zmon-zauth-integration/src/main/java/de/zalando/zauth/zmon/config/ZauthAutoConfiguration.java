63fd5734c62dfd8f3c7c962e67376ec1413a881c
Priyadarshini Maji <priyadarshini.maji@zalando.de>
2015-12-15 15:23:56 +0100

    Add Zauth authentication login

--- zmon-zauth-integration/src/main/java/de/zalando/zauth/zmon/config/ZauthAutoConfiguration.java
+++ zmon-zauth-integration/src/main/java/de/zalando/zauth/zmon/config/ZauthAutoConfiguration.java
@@ -1,24 +1,26 @@
 package de.zalando.zauth.zmon.config;
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
+
 import org.zalando.zmon.config.ZmonOAuth2Properties;
 import org.zalando.zmon.security.SigninController;
 
 /**
- * 
- * @author jbellmann
- *
+ * @author  jbellmann
  */
 @Configuration
 @EnableConfigurationProperties({ ZmonOAuth2Properties.class })
-@Import({ZauthSecurityConfig.class, ZauthSocialConfigurer.class})
+@Import({ ZauthSecurityConfig.class, ZauthSocialConfigurer.class })
+@Profile("zauth")
 public class ZauthAutoConfiguration {
-	
-	@Bean
-	public SigninController signinController(){
-		return new SigninController();
-	}
+
+    @Bean
+    public SigninController signinController() {
+        return new SigninController();
+    }
 }
