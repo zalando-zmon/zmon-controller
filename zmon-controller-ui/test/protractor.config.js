9f06d4be4df17d6941ca31f7af33ba8b57434519
Emanuel Lauria <emanuel.lauria@zalando.de>
2016-02-02 18:29:12 +0100

    update npm dependencies and scripts

--- zmon-controller-ui/test/protractor.config.js
+++ zmon-controller-ui/test/protractor.config.js
@@ -1,25 +1,17 @@
 exports.config = {
-    // The address of a running selenium server.
-    // seleniumAddress: 'http://localhost:4444/wd/hub',
 
-    seleniumServerJar: '../node_modules/protractor/selenium/selenium-server-standalone-2.47.1.jar',
+    specs: [
+        'e2e/*.js'
+    ],
 
-    chromeDriver: '../node_modules/protractor/selenium/chromedriver',
-
-    // Capabilities to be passed to the webdriver instance.
-    // If you want to use PhantomJS as browser please intall PhantomJS globally
-    // npm install -g phantomjs
     capabilities: {
         'browserName': 'chrome'
     },
 
-    // Spec patterns are relative to the location of the spec file. They may
-    // specs: ['./e2e/**/*.spec.js'],
+    chromeOnly: true,
 
-    // URL of project
     baseUrl: 'https://localhost:8443',
 
-    // Options to be passed to Jasmine-node.
     jasmineNodeOpts: {
         showColors: true,
         isVerbose: true,
