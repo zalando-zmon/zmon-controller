a577d1254bf63ad06d74c3bb0f0f2c6815cf0c1d
Joerg Bellmann <joerg.bellmann@zalando.de>
2016-02-05 16:55:28 +0100

    add realm '/employee'

--- zmon-github-integration/src/main/java/org/zalando/github/zmon/service/GithubResourceServerTokenServices.java
+++ zmon-github-integration/src/main/java/org/zalando/github/zmon/service/GithubResourceServerTokenServices.java
@@ -1,26 +1,26 @@
 package org.zalando.github.zmon.service;
 
-import com.google.common.collect.Sets;
-import de.zalando.zmon.security.AuthorityService;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.common.exceptions.InvalidTokenException;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.OAuth2Request;
 import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
-import org.springframework.social.github.api.GitHubUser;
 import org.springframework.social.github.api.impl.GitHubTemplate;
 import org.springframework.web.client.HttpClientErrorException;
 
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
+
+import de.zalando.zmon.security.AuthorityService;
 
 /**
  * Created by hjacobs on 14.12.15.
@@ -37,10 +37,12 @@ public class GithubResourceServerTokenServices implements ResourceServerTokenSer
     }
 
     @Override
-    public OAuth2Authentication loadAuthentication(String accessToken) throws AuthenticationException, InvalidTokenException {
+    public OAuth2Authentication loadAuthentication(String accessToken)
+            throws AuthenticationException, InvalidTokenException {
         GitHubTemplate tpl = new GitHubTemplate(accessToken);
 
-        // TODO: Important: we need to check the GitHub user's organization etc to comply with out configured "SignupConditions"
+        // TODO: Important: we need to check the GitHub user's organization etc
+        // to comply with out configured "SignupConditions"
         String username;
         try {
             username = tpl.userOperations().getProfileId();
@@ -52,11 +54,17 @@ public class GithubResourceServerTokenServices implements ResourceServerTokenSer
         }
         Collection<? extends GrantedAuthority> authorities = authorityService.getAuthorities(username);
 
-        UsernamePasswordAuthenticationToken user = new UsernamePasswordAuthenticationToken(username, "N/A", authorities);
-        //user.setDetails(map);
-        Set scopes = Sets.newHashSet("uid");
+        UsernamePasswordAuthenticationToken user = new UsernamePasswordAuthenticationToken(username, "N/A",
+                authorities);
 
-        OAuth2Request request = new OAuth2Request((Map)null, "NOT_NEEDED", (Collection)null, true, scopes, (Set)null, (String)null, (Set)null, (Map)null);
+        Set<String> scopes = Sets.newHashSet("uid");
+        Map<String, Object> map = Maps.newHashMap();
+        map.put("scopes", scopes);
+        map.put("realm", "/employee");
+        user.setDetails(map);
+
+        OAuth2Request request = new OAuth2Request((Map) null, "NOT_NEEDED", (Collection) null, true, scopes, (Set) null,
+                (String) null, (Set) null, (Map) null);
         return new OAuth2Authentication(request, user);
     }
 
