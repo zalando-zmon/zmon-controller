17fda11785141be8236d27c052f9ea377d43d8b4
Henning Jacobs <henning@jacobs1.de>
2016-02-09 14:30:34 +0100

    remove backendContext.xml

--- zmon-controller-app/src/main/java/de/zalando/zmon/config/XmlConfigFileConfiguration.java
+++ zmon-controller-app/src/main/java/de/zalando/zmon/config/XmlConfigFileConfiguration.java
@@ -5,13 +5,12 @@ import org.springframework.context.annotation.PropertySource;
 
 /**
  * Move step-by-step to replace the xml.
- * 
+ *
  * @author jbellmann
  *
  */
 @Configuration
 @PropertySource("classpath:/db_api_version.properties")
 @PropertySource("classpath:/zmon.properties")
-// @ImportResource({"classpath:/backendContext.xml"})
 public class XmlConfigFileConfiguration {
 }
