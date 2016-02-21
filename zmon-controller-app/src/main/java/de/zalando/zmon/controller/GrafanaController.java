7951963b52dbd0cef20c996574d55a05ea79ee57
Jan M <janm81@gmail.com>
2016-02-21 12:30:23 +0100

    Playing with thymeleaf for user details

--- zmon-controller-app/src/main/java/de/zalando/zmon/controller/GrafanaController.java
+++ zmon-controller-app/src/main/java/de/zalando/zmon/controller/GrafanaController.java
@@ -1,10 +1,15 @@
 package de.zalando.zmon.controller;
 
+import de.zalando.zmon.security.AuthorityService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class GrafanaController {
+
+    @Autowired
+    public AuthorityService authService;
 
     @RequestMapping(value = "/grafana")
     public String grafana() {
