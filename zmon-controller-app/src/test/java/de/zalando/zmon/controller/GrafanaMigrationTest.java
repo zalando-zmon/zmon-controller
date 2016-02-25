3beb21946e2925ead5c7807c75e82b9f38c2c34e
Jan M <janm81@gmail.com>
2016-02-25 16:24:52 +0100

    Basic test for migration

--- zmon-controller-app/src/test/java/de/zalando/zmon/controller/GrafanaMigrationTest.java
+++ zmon-controller-app/src/test/java/de/zalando/zmon/controller/GrafanaMigrationTest.java
@@ -1,6 +1,7 @@
 package de.zalando.zmon.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.junit.Test;
 import org.springframework.core.io.ClassPathResource;
@@ -18,5 +19,13 @@ public class GrafanaMigrationTest {
         ObjectNode target = (ObjectNode)mapper.readTree((new ClassPathResource("/old_grafana_target.json").getInputStream()));
         Grafana2Controller.migrateTarget(target);
         assert(target.get("tags") instanceof  ObjectNode);
+    }
+
+    @Test
+    public void testMigrationRow() throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectNode row = (ObjectNode)mapper.readTree((new ClassPathResource("/old_grafana_row.json").getInputStream()));
+        Grafana2Controller.migrateV1(row);
+        assert(row.get(0).get("targets").get(0).get("groupByTags") instanceof ArrayNode);
     }
 }
