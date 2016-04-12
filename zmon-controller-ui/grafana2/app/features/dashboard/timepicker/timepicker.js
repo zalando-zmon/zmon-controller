80ad73f52d806c3fa897f87d684963b748b791bb
Emanuel Lauria <emanuel.lauria@zalando.de>
2016-04-12 15:58:05 +0200

    #72 remove refresh intervals lower than 1m

--- zmon-controller-ui/grafana2/app/features/dashboard/timepicker/timepicker.js
+++ zmon-controller-ui/grafana2/app/features/dashboard/timepicker/timepicker.js
@@ -99,7 +99,7 @@ define(["require", "exports", "./input_date", 'angular', 'lodash', 'moment', 'ap
         TimePickerCtrl.tooltipFormat = 'MMM D, YYYY HH:mm:ss';
         TimePickerCtrl.defaults = {
             time_options: ['5m', '15m', '1h', '6h', '12h', '24h', '2d', '7d', '30d'],
-            refresh_intervals: ['5s', '10s', '30s', '1m', '5m', '15m', '30m', '1h', '2h', '1d'],
+            refresh_intervals: ['1m', '5m', '15m', '30m', '1h', '2h', '1d'],
         };
         return TimePickerCtrl;
     })();
