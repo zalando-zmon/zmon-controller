5c4e0e5908b2119e30c4452cdfab1814b015b12f
Henning Jacobs <henning@jacobs1.de>
2016-05-23 14:59:07 +0200

    remove obsolete scripts

--- create_squashed.sh
+++
@@ -1,5 +0,0 @@
-export IMAGE=zmon-controller
-
-docker build -t registry.opensource.zalan.do/stups/$IMAGE:$1-unsquashed .
-docker save registry.opensource.zalan.do/stups/$IMAGE:$1-unsquashed | docker-squash -verbose -t registry.opensource.zalan.do/stups/$IMAGE:$1 | docker load
-docker push registry.opensource.zalan.do/stups/$IMAGE:$1
