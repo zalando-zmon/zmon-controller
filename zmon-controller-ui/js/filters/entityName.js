c5ac63f6fb0dcdcd97224d8ecb9b3c16ade9f7c0
Emanuel Lauria <emanuel.lauria@zalando.de>
2016-05-25 12:44:05 +0200

    shorten entity IDs even more (comment)

--- zmon-controller-ui/js/filters/entityName.js
+++ zmon-controller-ui/js/filters/entityName.js
@@ -1,7 +1,7 @@
 angular.module('zmon2App').filter('entityName', function() {
     /*
      * Shortens entity names. Does nothing for entity names without square brackets.
-     * i.e: "i-09ec2da3[aws:786011980701:eu-west-1]" => "i-09ec2da3[a:701:eu-west-1]"
+     * i.e: "i-09ec2da3[aws:786011980701:eu-west-1]" => "i-09ec2da3[701:w]"
     */
     return function(value, length) {
         var r = /([^\[]+)\[([a-z]+):([a-z0-9]+)(:[^\]]+)?]/;
