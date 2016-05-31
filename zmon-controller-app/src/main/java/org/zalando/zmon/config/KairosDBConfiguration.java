0984a3fab958d789413f4afe000f60b1c205a699
Henning Jacobs <henning@jacobs1.de>
2016-05-31 16:32:59 +0200

    use proper KairosDB timeouts

--- zmon-controller-app/src/main/java/org/zalando/zmon/config/KairosDBConfiguration.java
+++ zmon-controller-app/src/main/java/org/zalando/zmon/config/KairosDBConfiguration.java
@@ -1,7 +1,15 @@
 package org.zalando.zmon.config;
 
+import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
+import org.apache.http.impl.nio.client.HttpAsyncClients;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsAsyncClientHttpRequestFactory;
+import org.springframework.http.client.Netty4ClientHttpRequestFactory;
+import org.springframework.http.client.OkHttpClientHttpRequestFactory;
+import org.springframework.web.client.AsyncRestTemplate;
 
 /**
  * Created by hjacobs on 1/28/16.
@@ -9,4 +17,22 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableConfigurationProperties({KairosDBProperties.class})
 public class KairosDBConfiguration {
+
+    @Autowired
+    KairosDBProperties kairosDBProperties;
+
+    /**
+     * Maybe we will need more configuration in the future, so let it do here.
+     *
+     * @return instead of using
+     *         {@link HttpComponentsAsyncClientHttpRequestFactory} you can also
+     *         use:
+     * @see Netty4ClientHttpRequestFactory
+     * @see OkHttpClientHttpRequestFactory
+     */
+    @Bean
+    public AsyncRestTemplate asyncRestTemplate() {
+        CloseableHttpAsyncClient client = kairosDBProperties.getHttpAsyncClient();
+        return new AsyncRestTemplate(new HttpComponentsAsyncClientHttpRequestFactory(client));
+    }
 }
