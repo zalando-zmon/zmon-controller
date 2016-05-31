d93ee9277951eb1162ee962c1e0c31032ec86853
Jan M <janm81@gmail.com>
2016-05-31 13:02:32 +0200

    Change link to grafana again (using v3). Fix missing migration issues. Added commented code for auth check

--- zmon-controller-app/src/main/java/org/zalando/zmon/controller/GrafanaController.java
+++ zmon-controller-app/src/main/java/org/zalando/zmon/controller/GrafanaController.java
@@ -100,18 +100,18 @@ public class GrafanaController extends AbstractZMonController {
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
     @RequestMapping(value = "/api/search", method = RequestMethod.GET)
-    public JsonNode searchDashboards(@RequestParam(value="query", required = false) String query, @RequestParam(value="tag", required = false) List<String> tags, @RequestParam(value="starred", defaultValue="false") boolean starred) throws IOException, ZMonException {
+    public JsonNode searchDashboards(@RequestParam(value = "query", required = false) String query, @RequestParam(value = "tag", required = false) List<String> tags, @RequestParam(value = "starred", defaultValue = "false") boolean starred) throws IOException, ZMonException {
         if (null == query) {
             query = "";
         }
 
         String starredBy = null;
-        if(starred) {
+        if (starred) {
             starredBy = authService.getUserName();
         }
 
         String jsonTags = null;
-        if(tags!=null && tags.size()>0) {
+        if (tags != null && tags.size() > 0) {
             jsonTags = mapper.writeValueAsString(tags);
         }
         LOG.info("Grafana search: query=\"{}\" starred={} by={} tags={}", query, starred, starredBy, jsonTags);
@@ -119,21 +119,20 @@ public class GrafanaController extends AbstractZMonController {
         List<GrafanaDashboardSprocService.GrafanaDashboard> results = grafanaService.getGrafanaDashboards(query, jsonTags, starredBy, authService.getUserName());
         ArrayNode resultsNode = mapper.createArrayNode();
 
-        for (GrafanaDashboardSprocService.GrafanaDashboard d : results ) {
+        for (GrafanaDashboardSprocService.GrafanaDashboard d : results) {
             ObjectNode dashboard = resultsNode.addObject();
-            dashboard.put("uri", "db/"+d.id);
+            dashboard.put("uri", "db/" + d.id);
             dashboard.put("id", d.id);
             dashboard.put("type", "db-dash");
             dashboard.put("title", d.title);
 
-            if(d.tags != null && !"".equals(d.tags) && !"[]".equals(d.tags)) {
+            if (d.tags != null && !"".equals(d.tags) && !"[]".equals(d.tags)) {
                 JsonNode tagsField = mapper.readTree(d.tags);
                 dashboard.set("tags", tagsField);
-            }
-            else {
+            } else {
                 dashboard.putArray("tags");
-                if(d.grafanaVersion.equals("v1")) {
-                    ArrayNode tagNode = ((ArrayNode)dashboard.get("tags"));
+                if (d.grafanaVersion.equals("v1")) {
+                    ArrayNode tagNode = ((ArrayNode) dashboard.get("tags"));
                     tagNode.add("v1");
                 }
             }
@@ -145,31 +144,31 @@ public class GrafanaController extends AbstractZMonController {
     }
 
     public static String getUnit(String unit) {
-        if(unit.equals("minutes")) return "m";
-        if(unit.equals("hours")) return "h";
-        if(unit.equals("seconds")) return "s";
+        if (unit.equals("minutes")) return "m";
+        if (unit.equals("hours")) return "h";
+        if (unit.equals("seconds")) return "s";
         return "m";
     }
 
     public static void migratePanel(ObjectNode panel) {
         panel.putNull("datasource");
         ArrayNode overrides = (ArrayNode) panel.get("seriesOverrides");
-        if(null != overrides && overrides.size() >0) {
-            for(int i = 0; i < overrides.size(); ++i) {
+        if (null != overrides && overrides.size() > 0) {
+            for (int i = 0; i < overrides.size(); ++i) {
                 ObjectNode o = (ObjectNode) overrides.get(i);
                 String id = o.get("alias").textValue();
-                o.put("alias", id.replace("{"," ( ").replace("}", " ) "));
+                o.put("alias", id.replace("{", " ( ").replace("}", " ) "));
             }
         }
     }
 
     public static void migrateSampling(ObjectNode target) {
-        if(target.has("sampling")) {
-            ObjectNode sampling = (ObjectNode)target.get("sampling");
+        if (target.has("sampling")) {
+            ObjectNode sampling = (ObjectNode) target.get("sampling");
             String unit = sampling.get("unit").textValue();
             String value = sampling.get("value").textValue();
 
-            target.put("sampling", value.replace("'","") + getUnit(unit));
+            target.put("sampling", value.replace("'", "") + getUnit(unit));
 
             ArrayNode h = target.putArray("horizontalAggregators");
             String aggregator = target.get("aggregator").textValue();
@@ -181,38 +180,43 @@ public class GrafanaController extends AbstractZMonController {
     }
 
     public static void migrateTarget(ObjectNode target) {// convert groups
-        if(target.get("groups")!=null && target.get("groups").size()>0) {
+        if (target.get("groups") != null && target.get("groups").size() > 0) {
             target.put("currentGroupByType", "tag");
-            ArrayNode groupTags = (ArrayNode)target.get("groups");
+            ArrayNode groupTags = (ArrayNode) target.get("groups");
             ArrayNode groupV2 = target.putArray("groupByTags");
-            for(int l = 0; l < groupTags.size(); ++l) {
+            for (int l = 0; l < groupTags.size(); ++l) {
                 groupV2.add(groupTags.get(l).textValue());
             }
-        }
-        else {
+        } else {
             target.remove("groups");
         }
 
-        if(!target.has("downsampling")) {
+        if (!target.has("downsampling")) {
             target.put("downsampling", "(NONE)");
         }
 
-        ArrayNode oldTags = (ArrayNode)target.get("tags");
-        ObjectNode newTags = target.putObject("tags");
+        if (null != target.get("tags") && target.get("tags") instanceof ArrayNode) {
+            ArrayNode oldTags = (ArrayNode) target.get("tags");
+            ObjectNode newTags = target.putObject("tags");
 
-        // convert tag filter
-        if(oldTags!=null && oldTags.size()>0) {
-            for(int t = 0; t < oldTags.size(); ++t) {
-                ObjectNode tf = (ObjectNode)oldTags.get(t);
-                String tk = tf.get("key").textValue();
-                String tv = tf.get("value").textValue();
-                if(newTags.has(tk)) {
-                    ArrayNode vs = (ArrayNode)newTags.get(tk);
-                    vs.add(tv);
-                }
-                else {
-                    ArrayNode vs = newTags.putArray(tk);
-                    vs.add(tv);
+            // convert tag filter
+            if (oldTags != null && oldTags.size() > 0) {
+                for (int t = 0; t < oldTags.size(); ++t) {
+                    ObjectNode tf = (ObjectNode) oldTags.get(t);
+                    if (null == tf || !tf.has("key") || !tf.has("value")) {
+                        continue;
+                    }
+
+                    String tk = tf.get("key").textValue();
+                    String tv = tf.get("value").textValue();
+
+                    if (newTags.has(tk)) {
+                        ArrayNode vs = (ArrayNode) newTags.get(tk);
+                        vs.add(tv);
+                    } else {
+                        ArrayNode vs = newTags.putArray(tk);
+                        vs.add(tv);
+                    }
                 }
             }
         }
@@ -221,21 +225,21 @@ public class GrafanaController extends AbstractZMonController {
     public static void migrateV1(ObjectNode dashboard) {
         LOG.info("Migrating dashboard to v2 properties");
         ArrayNode rows = (ArrayNode) dashboard.get("rows");
-        if (null!=rows) {
+        if (null != rows) {
             for (int i = 0; i < rows.size(); ++i) {
                 JsonNode row = rows.get(i);
-                if (null!=row && row.has("panels")) {
+                if (null != row && row.has("panels")) {
                     ArrayNode panels = (ArrayNode) row.get("panels");
                     if (null == panels) continue;
 
                     for (int j = 0; j < panels.size(); ++j) {
-                        if(null==panels.get(j)) continue;
+                        if (null == panels.get(j)) continue;
                         ObjectNode panel = (ObjectNode) panels.get(j);
                         migratePanel(panel);
-                        ArrayNode targets = (ArrayNode)panel.get("targets");
+                        ArrayNode targets = (ArrayNode) panel.get("targets");
 
-                        for(int k = 0; null!=targets && k < targets.size(); ++k) {
-                            ObjectNode target = (ObjectNode)targets.get(k);
+                        for (int k = 0; null != targets && k < targets.size(); ++k) {
+                            ObjectNode target = (ObjectNode) targets.get(k);
                             migrateTarget(target);
                             migrateSampling(target);
                         }
@@ -249,15 +253,15 @@ public class GrafanaController extends AbstractZMonController {
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
     @RequestMapping(value = "/api/dashboards/db/{id}", method = RequestMethod.GET)
-    public ResponseEntity<JsonNode> getDashboard(@PathVariable(value="id") String id) throws ZMonException, IOException {
-        if(null == id || "".equals(id)) {
+    public ResponseEntity<JsonNode> getDashboard(@PathVariable(value = "id") String id) throws ZMonException, IOException {
+        if (null == id || "".equals(id)) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
 
         id = id.toLowerCase();
 
         List<GrafanaDashboardSprocService.GrafanaDashboard> dashboards = grafanaService.getGrafanaDashboard(id, authService.getUserName());
-        if(dashboards.size()==0) {
+        if (dashboards.size() == 0) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
 
@@ -277,12 +281,12 @@ public class GrafanaController extends AbstractZMonController {
         meta.put("slug", id);
         meta.put("isStarred", dashboard.starred);
 
-        ObjectNode model =  (ObjectNode) mapper.readTree(dashboard.dashboard);
+        ObjectNode model = (ObjectNode) mapper.readTree(dashboard.dashboard);
         model.put("id", id);
 
-        if(model.has("refresh") && model.get("refresh") != null) {
+        if (model.has("refresh") && model.get("refresh") != null) {
             String refresh = model.get("refresh").textValue();
-            if (refresh!=null && refresh.endsWith("s")) {
+            if (refresh != null && refresh.endsWith("s")) {
                 int interval = Integer.parseInt(refresh.replace("s", ""));
                 if (interval < controllerProperties.getGrafanaMinInterval()) {
                     model.put("refresh", controllerProperties.getGrafanaMinInterval() + "s");
@@ -290,7 +294,7 @@ public class GrafanaController extends AbstractZMonController {
             }
         }
 
-        if(dashboard.grafanaVersion.equals("v1")) {
+        if (dashboard.grafanaVersion.equals("v1")) {
             migrateV1(model);
         }
 
@@ -302,7 +306,7 @@ public class GrafanaController extends AbstractZMonController {
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
     @RequestMapping(value = "/api/dashboards/db/{id}", method = RequestMethod.DELETE)
-    public ResponseEntity<JsonNode> deleteG2Dashboard(@PathVariable(value="id") String id) {
+    public ResponseEntity<JsonNode> deleteG2Dashboard(@PathVariable(value = "id") String id) {
         grafanaService.deleteGrafanaDashboard(id, authService.getUserName());
         return new ResponseEntity<>(HttpStatus.OK);
     }
@@ -311,9 +315,13 @@ public class GrafanaController extends AbstractZMonController {
     @ResponseBody
     @RequestMapping(value = "/api/dashboards/db", method = RequestMethod.POST, produces = "application/json")
     public ResponseEntity<JsonNode> saveDashboard(@RequestBody(required = true) final JsonNode grafanaData) throws IOException {
-
+        /*
+        if(authService.hasOnetimeTokenPrivilege()) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+         */
         String title = grafanaData.get("dashboard").get("title").textValue();
-        if(title==null || "".equals(title)) {
+        if (title == null || "".equals(title)) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
 
@@ -321,9 +329,8 @@ public class GrafanaController extends AbstractZMonController {
 
         String id = grafanaData.get("dashboard").get("id").textValue();
         if (null == id || "".equals(id)) {
-            id = title.replace(" ", "-").replace("'","").toLowerCase();
-        }
-        else {
+            id = title.replace(" ", "-").replace("'", "").toLowerCase();
+        } else {
             id = id.toLowerCase();
         }
 
@@ -383,7 +390,7 @@ public class GrafanaController extends AbstractZMonController {
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
     @RequestMapping(value = "/api/datasources/{id}", method = RequestMethod.GET)
-    public JsonNode g2getDatasource(@PathVariable(value="id") String id) throws ZMonException {
+    public JsonNode g2getDatasource(@PathVariable(value = "id") String id) throws ZMonException {
         ObjectNode k = mapper.createObjectNode();
         k.put("id", 1);
         k.put("orgId", "1");
@@ -459,13 +466,13 @@ public class GrafanaController extends AbstractZMonController {
     }
 
 
-    @RequestMapping(value="/api/user/orgs", method = RequestMethod.GET)
+    @RequestMapping(value = "/api/user/orgs", method = RequestMethod.GET)
     @ResponseBody
     public Collection<String> getUserOrgs() {
         return authService.getTeams();
     }
 
-    @RequestMapping(value="/api/user", method = RequestMethod.GET)
+    @RequestMapping(value = "/api/user", method = RequestMethod.GET)
     @ResponseBody
     public JsonNode getCurrentUser() {
         ObjectNode node = mapper.createObjectNode();
@@ -477,12 +484,12 @@ public class GrafanaController extends AbstractZMonController {
         return node;
     }
 
-    @RequestMapping(value="/api/dashboards/tags")
+    @RequestMapping(value = "/api/dashboards/tags")
     @ResponseBody
     public JsonNode getGrafanaTags() {
         ArrayNode result = mapper.createArrayNode();
         List<GrafanaDashboardSprocService.GrafanaTag> tags = grafanaService.getTagsWithCount();
-        for(GrafanaDashboardSprocService.GrafanaTag t : tags) {
+        for (GrafanaDashboardSprocService.GrafanaTag t : tags) {
             ObjectNode node = result.addObject();
             node.put("term", t.tag);
             node.put("count", t.count);
@@ -490,14 +497,14 @@ public class GrafanaController extends AbstractZMonController {
         return result;
     }
 
-    @RequestMapping(value="/api/user/stars/dashboard/{id}", method=RequestMethod.POST)
+    @RequestMapping(value = "/api/user/stars/dashboard/{id}", method = RequestMethod.POST)
     @ResponseBody
     public JsonNode starDashboard(@PathVariable String id) {
         grafanaService.starGrafanaDashboard(id, authService.getUserName());
         return mapper.createObjectNode();
     }
 
-    @RequestMapping(value="/api/user/stars/dashboard/{id}", method=RequestMethod.DELETE)
+    @RequestMapping(value = "/api/user/stars/dashboard/{id}", method = RequestMethod.DELETE)
     @ResponseBody
     public JsonNode unstarDashboard(@PathVariable String id) {
         grafanaService.unstarGrafanaDashboard(id, authService.getUserName());
