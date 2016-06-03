f663211514e2d77e96cc0c7c1c682c2b073ab01e
Henning Jacobs <henning@jacobs1.de>
2016-06-03 12:00:38 +0200

    #147 fix Grafana 3 login redirect

--- zmon-security-common/src/main/java/org/zalando/zmon/security/WebSecurityConstants.java
+++ zmon-security-common/src/main/java/org/zalando/zmon/security/WebSecurityConstants.java
@@ -4,7 +4,7 @@ public class WebSecurityConstants {
 
     public static String[] IGNORED_PATHS = new String[]{
             "/logo.png", "/favicon.ico", "/asset/**", "/styles/**", "/js/**", "/lib/**",
-            "/grafana/**",
+            "/grafana/public/**",
             "/grafana2/vendor/**", "/grafana2/img/**", "/grafana2/css/**", "/grafana2/fonts/**",
             "/tv/**"};
 }
