7a69345ac8a88236f4628439000d24b230460663
Emanuel Lauria <emanuel.lauria@zalando.de>
2016-06-06 15:05:59 +0200

    #165 remove ngFocus leftovers on autofocus directive

--- zmon-controller-ui/js/directives/autofocus.js
+++ zmon-controller-ui/js/directives/autofocus.js
@@ -1,9 +1,7 @@
 angular.module('zmon2App').directive('autofocus', function($timeout) {
     return {
         link: function ( scope, element, attrs ) {
-            scope.$watch( attrs.ngFocus, function ( val ) {
-                    $timeout( function () { element[0].focus(); } );
-            }, true);
+            $timeout( function () { element[0].focus(); } );
         }
     };
 });
