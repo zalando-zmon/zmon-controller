86b47301aeb6da7fc1331587a27445b3c46cb995
Jan M <janm81@gmail.com>
2016-07-04 14:21:05 +0200

    Fix PostgreSQL signature and function to work with array in string

--- database/zmon/20_api/05_stored_procedures/13_get_entities.sql
+++ database/zmon/20_api/05_stored_procedures/13_get_entities.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION get_entities(filter text[]) RETURNS SETOF jsonb AS
+CREATE OR REPLACE FUNCTION get_entities(filter text) RETURNS SETOF jsonb AS
 $$
- SELECT e_data FROM zzm_data.entity WHERE e_data @> ANY ( filter::jsonb[] )
+ SELECT e_data FROM zzm_data.entity WHERE e_data @> ANY ( ARRAY( SELECT jsonb_array_elements(filter) ) )
 $$ LANGUAGE 'sql' VOLATILE SECURITY DEFINER;
