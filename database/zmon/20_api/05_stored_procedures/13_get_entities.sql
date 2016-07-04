3ac3dc88d91822d0910d90cec9e9b4e634ef355b
Jan M <janm81@gmail.com>
2016-07-04 14:31:34 +0200

    Solve last type cast

--- database/zmon/20_api/05_stored_procedures/13_get_entities.sql
+++ database/zmon/20_api/05_stored_procedures/13_get_entities.sql
@@ -1,4 +1,4 @@
 CREATE OR REPLACE FUNCTION get_entities(filter text) RETURNS SETOF jsonb AS
 $$
- SELECT e_data FROM zzm_data.entity WHERE e_data @> ANY ( ARRAY( SELECT jsonb_array_elements(filter) ) )
+ SELECT e_data FROM zzm_data.entity WHERE e_data @> ANY ( ARRAY( SELECT jsonb_array_elements(filter::jsonb) ) )
 $$ LANGUAGE 'sql' VOLATILE SECURITY DEFINER;
