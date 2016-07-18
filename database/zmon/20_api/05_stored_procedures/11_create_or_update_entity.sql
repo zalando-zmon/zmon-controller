eb415941f9d92631dff0336043463087671083cd
Jan M <janm81@gmail.com>
2016-07-18 12:26:04 +0200

    Fix sproc, to detect unmodified and return ID

--- database/zmon/20_api/05_stored_procedures/11_create_or_update_entity.sql
+++ database/zmon/20_api/05_stored_procedures/11_create_or_update_entity.sql
@@ -2,7 +2,13 @@ CREATE OR REPLACE FUNCTION create_or_update_entity(entity_data text, teams text[
 $$
 DECLARE
   _id text;
+  _data jsonb;
 BEGIN
+  SELECT e_data INTO _data FROM zzm_data.entity WHERE (e_data->'id')::text = ((entity_data::jsonb)->'id')::text;
+  IF _data IS NOT DISTINCT FROM entity_data::jsonb THEN
+    RETURN (_data->'id')::text;
+  END IF;
+
   BEGIN
     INSERT INTO zzm_data.entity(e_data, e_created_by, e_last_modified_by) SELECT entity_data::jsonb, user_name, user_name RETURNING (e_data->'id')::text INTO _id;
   EXCEPTION WHEN UNIQUE_VIOLATION THEN
