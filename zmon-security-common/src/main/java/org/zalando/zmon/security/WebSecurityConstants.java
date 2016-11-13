e9ce7b322bab308e89bd2ed4d0693c6675fc46d3
Jan M <janm81@gmail.com>
2016-11-13 21:16:22 +0100

    allow ro read notification images

--- zmon-security-common/src/main/java/org/zalando/zmon/security/WebSecurityConstants.java
+++ zmon-security-common/src/main/java/org/zalando/zmon/security/WebSecurityConstants.java
@@ -3,7 +3,7 @@ package org.zalando.zmon.security;
 public class WebSecurityConstants {
 
     public static String[] IGNORED_PATHS = new String[]{
-            "/logo.png", "/favicon.ico", "/asset/**", "/styles/**", "/js/**", "/lib/**",
+            "/logo.png","/clean.png","/warning.png", "/favicon.ico", "/asset/**", "/styles/**", "/js/**", "/lib/**",
             "/grafana/public/**",
             "/tv/**"};
 }
