e5a0a926acb08f8127f7ae67829bd7691d4420dc
Jan M <janm81@gmail.com>
2017-01-20 18:22:26 +0100

    support most basic ! for tags

--- database/zmon/20_api/05_stored_procedures/30_get_alert_definitions_by_team_and_tag.sql
+++ database/zmon/20_api/05_stored_procedures/30_get_alert_definitions_by_team_and_tag.sql
@@ -4,7 +4,13 @@ CREATE OR REPLACE FUNCTION get_alert_definitions_by_team_and_tag(
      IN tags                text[]
 ) RETURNS SETOF alert_definition_type AS
 $BODY$
+DECLARE
+  include_tags text[];
+  exclude_tags text[];
 BEGIN
+    include_tags := ARRAY(SELECT t FROM unnest(tags) t(t) WHERE t not like '!%');
+    exclude_tags := ARRAY(SELECT substring(t from 2) FROM unnest(tags) t(t) WHERE t like '!%');
+
     RETURN QUERY
         SELECT ad_id,
                ad_name,
@@ -28,7 +34,8 @@ BEGIN
           FROM zzm_data.alert_definition
          WHERE (status IS NULL OR ad_status = status)
            AND (teams IS NULL OR ad_team ILIKE ANY (teams))
-           AND (tags IS NULL OR tags && ad_tags);
+           AND (include_tags IS NULL OR array[]::text[] = include_tags OR include_tags && ad_tags)
+           AND NOT (exclude_tags IS NOT NULL AND exclude_tags && ad_tags);
 END
 $BODY$
 LANGUAGE 'plpgsql' VOLATILE SECURITY DEFINER
