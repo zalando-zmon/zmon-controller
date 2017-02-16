51e42417ee2c315787a2beb6f0d96124bee4d3a1
Jan M <janm81@gmail.com>
2017-02-16 19:16:45 +0100

    Consistent now

--- database/zmon/20_api/05_stored_procedures/60_onetime_access_tokens.sql
+++ database/zmon/20_api/05_stored_procedures/60_onetime_access_tokens.sql
@@ -27,7 +27,7 @@ $$
      AND (oat_bound_ip is NULL OR oat_bound_ip = bind_ip)
      AND (oat_bound_session_id is NULL OR oat_bound_session_id = session_id)
      -- check initial token validity for using the token to create session
-     AND (oat_valid_until > NOW() OR oat_bound_session_id = session_id)
+     AND (oat_valid_until > now() OR oat_bound_session_id = session_id)
      -- check that bound session is not expired yet
      AND (oat_bound_expires > now())
     RETURNING oat_token, oat_created, oat_bound_at, oat_bound_ip, oat_bound_expires;
