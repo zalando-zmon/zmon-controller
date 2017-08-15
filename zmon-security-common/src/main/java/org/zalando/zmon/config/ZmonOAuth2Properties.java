c7f15e90b3fc5c036c2e174a988d55dd927253ac
Ivan Kirichenko <iddqd07@yandex.ru>
2017-08-15 17:52:46 +0200

    Removes ChainCredentialsSupplier as it's not needed anymore
    - adds new properties in config to support new version of spring-social-zauth

--- zmon-security-common/src/main/java/org/zalando/zmon/config/ZmonOAuth2Properties.java
+++ zmon-security-common/src/main/java/org/zalando/zmon/config/ZmonOAuth2Properties.java
@@ -11,14 +11,22 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 public class ZmonOAuth2Properties {
 
 	private String clientId;
-
 	private String clientSecret;
 
-    /**
-     * Path to the directory where credential-files can be found (client.json).
-     * 
-     */
-    private String credentialsDirectoryPath;
+	private String authorizeUrl;
+	private String accessTokenUrl;
+
+	/**
+	 * Path to the directory where credential-files can be found (client.json).
+	 */
+	private String credentialsDirectoryPath;
+
+	/**
+	 * Name of the token used by the Platform
+	 */
+	private String platformTokenName;
+
+	private boolean platformEnabled;
 
 	public String getClientId() {
 		return clientId;
@@ -42,6 +50,38 @@ public class ZmonOAuth2Properties {
 
     public void setCredentialsDirectoryPath(String credentialsDirectoryPath) {
         this.credentialsDirectoryPath = credentialsDirectoryPath;
-    }
+	}
 
+
+	public String getAuthorizeUrl() {
+		return authorizeUrl;
+	}
+
+	public void setAuthorizeUrl(final String authorizeUrl) {
+		this.authorizeUrl = authorizeUrl;
+	}
+
+	public String getAccessTokenUrl() {
+		return accessTokenUrl;
+	}
+
+	public void setAccessTokenUrl(final String accessTokenUrl) {
+		this.accessTokenUrl = accessTokenUrl;
+	}
+
+	public String getPlatformTokenName() {
+		return platformTokenName;
+	}
+
+	public void setPlatformTokenName(final String platformTokenName) {
+		this.platformTokenName = platformTokenName;
+	}
+
+	public boolean isPlatformEnabled() {
+		return platformEnabled;
+	}
+
+	public void setPlatformEnabled(final boolean platformEnabled) {
+		this.platformEnabled = platformEnabled;
+	}
 }
