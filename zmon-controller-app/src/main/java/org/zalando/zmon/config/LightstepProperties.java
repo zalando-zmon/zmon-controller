c1558e5135a65d0ac6684c245385b710ee7a8eb1
Alex Beverage <abeverage@zalando-26362.corp.ad.zalando.net>
2017-12-12 16:33:23 +0100

    Add Jaeger config for POC

--- zmon-controller-app/src/main/java/org/zalando/zmon/config/LightstepProperties.java
+++ zmon-controller-app/src/main/java/org/zalando/zmon/config/LightstepProperties.java
@@ -1,7 +1,9 @@
 package org.zalando.zmon.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @ConfigurationProperties(prefix = "zmon.lightstep")
 public class LightstepProperties {
     public String accessToken;
