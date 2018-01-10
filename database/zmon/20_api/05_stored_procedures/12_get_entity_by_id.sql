b518b27ce02c4f3f6767544150e686d1eb5fbee8
Travis CI <builds@travis-ci.com>
2018-01-10 10:59:16 +0100

    include "created" field in entity

--- database/zmon/20_api/05_stored_procedures/12_get_entity_by_id.sql
+++ database/zmon/20_api/05_stored_procedures/12_get_entity_by_id.sql
@@ -1,4 +1,4 @@
 CREATE OR REPLACE FUNCTION get_entity_by_id(id text) RETURNS SETOF jsonb AS
 $$
-  SELECT e_data  || ('{"last_modified": "' || e_last_modified::text || '"}')::jsonb FROM zzm_data.entity WHERE (e_data->'id')::text = '"'||id||'"';
+  SELECT e_data  || ('{"last_modified": "' || e_last_modified::text || '", "created": "' || e_created::text || '"}')::jsonb FROM zzm_data.entity WHERE (e_data->'id')::text = '"'||id||'"';
 $$ LANGUAGE SQL VOLATILE SECURITY DEFINER;
