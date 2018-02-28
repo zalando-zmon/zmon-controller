4ba512e8f65745dbf9c0814069ad555bd130beb2
Rajat Parida <rajat.parida@zalando.de>
2018-02-28 14:28:20 +0100

    Injected opentracing config environment variables for grafana through grafanauicontroller class

--- zmon-controller-ui/lib/opentracing-javascript-utils/opentracing-javascript-utils.js
+++ zmon-controller-ui/lib/opentracing-javascript-utils/opentracing-javascript-utils.js
@@ -6,7 +6,7 @@
   const libraries = {
     lightstep: {
       class: 'lightstep',
-      src: 'https://rawgit.com/lightstep/lightstep-tracer-javascript/v0.20.3/dist/lightstep-tracer.min.js',
+      src: 'https://rawgit.com/zalando-zmon/opentracing-javascript-utils/master/lightstep-tracer.js',
     }
   };
 
