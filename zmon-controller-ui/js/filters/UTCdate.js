a283d646072cd84665f2dbd6aec530d6d8b8c6a3
Ankita Rajain <ankita.rajain@zalando.de>
2018-09-23 21:49:53 +0200

    changed 12hrs to 24 hrs time

--- zmon-controller-ui/js/filters/UTCdate.js
+++ zmon-controller-ui/js/filters/UTCdate.js
@@ -2,8 +2,7 @@ angular.module('zmon2App').filter('UTC', ['$filter', function($filter) {
     return function(localdate) {
        
         localdate = new Date(localdate);
-
         var utcdate = new Date(localdate.getUTCFullYear(), localdate.getUTCMonth(), localdate.getUTCDate(),  localdate.getUTCHours(), localdate.getUTCMinutes(), localdate.getUTCSeconds());
-        return utcdate;
+        return utcdate.toString().substr(4,20);        
     };
 }]);
