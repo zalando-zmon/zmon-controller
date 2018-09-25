4881d107f5ea39b3e843ca3fb5d907014e8bc9f9
vgoyal <vibhor.goyal@zalando.de>
2018-09-25 14:49:33 +0200

    added getters and setters for sentry properties

--- zmon-controller-app/src/main/java/org/zalando/zmon/config/SentryProperties.java
+++ zmon-controller-app/src/main/java/org/zalando/zmon/config/SentryProperties.java
@@ -13,4 +13,59 @@ public class SentryProperties {
     private String servername;
     private String tags;
 
+    public String getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(String enabled) {
+        this.enabled = enabled;
+    }
+
+    public String getDsn() {
+        return dsn;
+    }
+
+    public void setDsn(String dsn) {
+        this.dsn = dsn;
+    }
+
+    public String getDsnUI() {
+        return dsnUI;
+    }
+
+    public void setDsnUI(String dsnUI) {
+        this.dsnUI = dsnUI;
+    }
+
+    public String getSampleRate() {
+        return sampleRate;
+    }
+
+    public void setSampleRate(String sampleRate) {
+        this.sampleRate = sampleRate;
+    }
+
+    public String getEnvironment() {
+        return environment;
+    }
+
+    public void setEnvironment(String environment) {
+        this.environment = environment;
+    }
+
+    public String getServername() {
+        return servername;
+    }
+
+    public void setServername(String servername) {
+        this.servername = servername;
+    }
+
+    public String getTags() {
+        return tags;
+    }
+
+    public void setTags(String tags) {
+        this.tags = tags;
+    }
 }
