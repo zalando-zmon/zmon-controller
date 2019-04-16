8e89d8abf740bec32905f7ae2f8b0e3de9451e62
Alex Korotkikh <me@alexkorotkikh.com>
2019-04-16 10:56:54 +0200

    log checkId and dashboard name for each request to KDB (#684)

--- zmon-controller-app/src/main/java/org/zalando/zmon/controller/MultiKairosDBController.java
+++ zmon-controller-app/src/main/java/org/zalando/zmon/controller/MultiKairosDBController.java
@@ -15,15 +15,17 @@ import org.springframework.web.client.AsyncRestTemplate;
 import org.zalando.stups.tokens.AccessTokens;
 import org.zalando.zmon.config.KairosDBProperties;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
 /**
  * NOTE: we inject the AUTHORIZATION-HEADER manually. Better would be to use an
  * Interceptor. But this is coming with Spring 4.3,<br/>
- * {@link https://jira.spring.io/browse/SPR-12538}
+ * {@see <a href="https://jira.spring.io/browse/SPR-12538">docs</a>}
  *
  * @author jbellmann
  */
@@ -69,66 +71,47 @@ public class MultiKairosDBController extends AbstractZMonController {
      * URLs too
      */
     @RequestMapping(value = "{kairosdbId}/api/v1/datapoints/query", method = RequestMethod.POST, produces = "application/json")
-    public ListenableFuture<ResponseEntity<JsonNode>> kairosDBPost(@RequestBody(required = true) final JsonNode node, @PathVariable(value = "kairosdbId") String kairosDB) {
-
+    public ListenableFuture<ResponseEntity<JsonNode>> kairosDBPost(@RequestBody final JsonNode node,
+                                                                   @PathVariable(value = "kairosdbId") final String kairosDB,
+                                                                   final HttpServletRequest req) {
         if (!kairosdbServices.containsKey(kairosDB)) {
             return null;
         }
 
         KairosDBProperties.KairosDBServiceConfig kairosProperties = kairosdbServices.get(kairosDB);
 
-
         final String checkId = node.get("metrics").get(0).get("name").textValue().replace("zmon.check.", "");
         Timer.Context timer = metricRegistry.timer("kairosdb.check.query." + checkId).time();
 
-        final int queryWindow = kairosProperties.getMaxWindowLength();
+        alignQueriesToFullMinutes(node, kairosProperties.getMaxWindowLength());
 
-        // align all queries to full minutes
-        if (node instanceof ObjectNode) {
-            ObjectNode q = (ObjectNode) node;
-            q.put("cache_time", 60);
-            if (q.has("start_absolute")) {
-                long start = q.get("start_absolute").asLong();
-                start = start - (start % 60000);
-                q.put("start_absolute", start);
-            }
-            else if (q.has("start_relative")) {
-                if(queryWindow != 0) {
-                    ObjectNode r = (ObjectNode)q.get("start_relative");
-                    r.put("value", queryWindow);
-                    r.put("unit", "minutes");
-                }
-            }
-        }
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpHeaders headers = prepareHeaders(kairosDB);
         headers.add("X-ZMON-CHECK-ID", checkId);
 
-        if (kairosdbServices.get(kairosDB).isOauth2()) {
-            headers.add(AUTHORIZATION, BEARER + accessTokens.get(KAIROSDB_TOKEN_ID));
-        }
+        Optional.ofNullable(req.getHeader("Referer")).ifPresent(referer -> {
+            final String[] parts = referer.split("/");
+            if (parts.length > 1) {
+                final String dashboard = parts[parts.length - 1];
+                log.info("datapoints_query checkId=" + checkId + " dashboard=" + dashboard);
+            }
+        });
 
         HttpEntity<String> httpEntity = new HttpEntity<>(node.toString(), headers);
 
-        ListenableFuture<ResponseEntity<JsonNode>> lf = asyncRestTemplate.exchange(kairosdbServices.get(kairosDB).getUrl() + QUERY_SUFFIX, HttpMethod.POST,
-                httpEntity, JsonNode.class);
+        ListenableFuture<ResponseEntity<JsonNode>> lf = asyncRestTemplate.exchange(
+                kairosdbServices.get(kairosDB).getUrl() + QUERY_SUFFIX, HttpMethod.POST, httpEntity, JsonNode.class);
         lf.addCallback(new StopTimerCallback(timer));
-
         return lf;
     }
 
     @RequestMapping(value = "{kairosdbId}/api/v1/datapoints/query/tags", method = RequestMethod.POST, produces = "application/json")
-    public ListenableFuture<ResponseEntity<JsonNode>> kairosDBtags(@RequestBody(required = true) final JsonNode node, @PathVariable(value = "kairosdbId") String kairosDB) {
+    public ListenableFuture<ResponseEntity<JsonNode>> kairosDBtags(@RequestBody final JsonNode node,
+                                                                   @PathVariable(value = "kairosdbId") String kairosDB) {
         if (!kairosdbServices.containsKey(kairosDB)) {
             return null;
         }
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        if (kairosdbServices.get(kairosDB).isOauth2()) {
-            headers.add(AUTHORIZATION, BEARER + accessTokens.get(KAIROSDB_TOKEN_ID));
-        }
+        HttpHeaders headers = prepareHeaders(kairosDB);
 
         HttpEntity<String> httpEntity = new HttpEntity<>(node.toString(), headers);
 
@@ -141,15 +124,38 @@ public class MultiKairosDBController extends AbstractZMonController {
             return null;
         }
 
+        HttpHeaders headers = prepareHeaders(kairosDB);
+
+        HttpEntity<String> httpEntity = new HttpEntity<>(headers);
+
+        return asyncRestTemplate.exchange(kairosdbServices.get(kairosDB).getUrl() + METRIC_NAMES_SUFFIX, HttpMethod.GET, httpEntity, JsonNode.class);
+    }
+
+    private HttpHeaders prepareHeaders(final String kairosDB) {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         if (kairosdbServices.get(kairosDB).isOauth2()) {
             headers.add(AUTHORIZATION, BEARER + accessTokens.get(KAIROSDB_TOKEN_ID));
         }
+        return headers;
+    }
 
-        HttpEntity<String> httpEntity = new HttpEntity<>(headers);
-
-        return asyncRestTemplate.exchange(kairosdbServices.get(kairosDB).getUrl() + METRIC_NAMES_SUFFIX, HttpMethod.GET, httpEntity, JsonNode.class);
+    private void alignQueriesToFullMinutes(final JsonNode node, final int queryWindow) {
+        if (node instanceof ObjectNode) {
+            ObjectNode q = (ObjectNode) node;
+            q.put("cache_time", 60);
+            if (q.has("start_absolute")) {
+                long start = q.get("start_absolute").asLong();
+                start = start - (start % 60000);
+                q.put("start_absolute", start);
+            } else if (q.has("start_relative")) {
+                if (queryWindow != 0) {
+                    ObjectNode r = (ObjectNode) q.get("start_relative");
+                    r.put("value", queryWindow);
+                    r.put("unit", "minutes");
+                }
+            }
+        }
     }
 
     static class StopTimerCallback implements ListenableFutureCallback<Object> {
