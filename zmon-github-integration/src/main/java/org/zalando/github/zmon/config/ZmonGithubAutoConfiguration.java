85273243df6e63b82ea1514ad022bf0ec6f7e7ec
☃ pitr <peter.vernigorov@zalando.de>
2019-06-18 12:58:14 +0200

    declare TaskScheduler bean in ZmonGithubAutoConfiguration

--- zmon-github-integration/src/main/java/org/zalando/github/zmon/config/ZmonGithubAutoConfiguration.java
+++ zmon-github-integration/src/main/java/org/zalando/github/zmon/config/ZmonGithubAutoConfiguration.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
 import org.zalando.github.zmon.security.GithubSignupConditionProperties;
 
 import org.zalando.zmon.config.ZmonOAuth2Properties;
@@ -29,5 +31,10 @@ public class ZmonGithubAutoConfiguration {
     @Bean
     public SigninController signinController() {
         return new SigninController();
+    }
+
+    @Bean
+    public TaskScheduler threadPoolTaskScheduler() {
+        return new ConcurrentTaskScheduler();
     }
 }
