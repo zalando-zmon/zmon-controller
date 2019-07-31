44d169dbcd82b36f6e17e0d64062a49173667562
Maxim Tschumak <maxim.tschumak@gmail.com>
2019-07-31 10:47:11 +0200

    fix: add getter/setter for converter (#712)

--- zmon-controller-app/src/main/java/org/zalando/zmon/api/domain/AlertResults.java
+++ zmon-controller-app/src/main/java/org/zalando/zmon/api/domain/AlertResults.java
@@ -3,9 +3,17 @@ package org.zalando.zmon.api.domain;
 import java.util.List;
 
 public class AlertResults {
-    private final List<AlertResult> alertResults;
+    private List<AlertResult> alertResults;
 
     public AlertResults(List<AlertResult> alertResults) {
+        this.alertResults = alertResults;
+    }
+
+    public List<AlertResult> getAlertResults() {
+        return alertResults;
+    }
+
+    public void setAlertResults(List<AlertResult> alertResults) {
         this.alertResults = alertResults;
     }
 }
