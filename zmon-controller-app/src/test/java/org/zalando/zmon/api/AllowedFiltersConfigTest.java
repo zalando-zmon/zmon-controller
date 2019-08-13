2c6d4e1685bdbcc46fda7cb6c00bcbe4479d5f97
Gregor Zeitlinger <gregor.zeitlinger@zalando.de>
2019-08-13 17:03:38 +0200

    fix: revert to @ConfigurationProperties (#712)

--- zmon-controller-app/src/test/java/org/zalando/zmon/api/AllowedFiltersConfigTest.java
+++ zmon-controller-app/src/test/java/org/zalando/zmon/api/AllowedFiltersConfigTest.java
@@ -2,7 +2,8 @@ package org.zalando.zmon.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.assertj.core.api.Assertions;
-import org.junit.Ignore;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,14 +14,8 @@ import org.zalando.zmon.service.AlertService;
 import org.zalando.zmon.service.ZMonService;
 import redis.clients.jedis.JedisPool;
 
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.MOCK;
-
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = {AlertStatusAPI.class},
-    webEnvironment = MOCK,
-    //yaml and json it's a list
-    properties = {"zmon.alert-results.allowed-filters=a,b"})
-@Ignore("doesn't work: if you want to try it locally, add the following to the environment variables: SPRING_APPLICATION_JSON={\"zmon\": { \"alert-results\": {\"allowed-filters\": [\"c\",\"d\"]}}}")
+@SpringBootTest(classes = {AlertStatusAPI.class})
 public class AllowedFiltersConfigTest {
 
     @MockBean
@@ -37,6 +32,16 @@ public class AllowedFiltersConfigTest {
 
     @Autowired
     private AlertStatusAPI alertStatusAPI;
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        System.setProperty("spring.application.json", "{\"zmon\": { \"alert-results\": {\"allowed-filters\": [\"c\",\"d\"]}}}");
+    }
+
+    @AfterClass
+    public static void tearDown() throws Exception {
+        System.clearProperty("spring.application.json");
+    }
 
     @Test
     public void testAllowedFilters() {
