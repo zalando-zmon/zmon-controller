96ed4b04ce8d9699d5d8a3181d117256319be80e
lfroment0 <loic.p.a.froment@gmail.com>
2019-11-04 17:33:02 +0100

    fix service name

--- zmon-controller-ui/js/services/ConfigurationService.js
+++ zmon-controller-ui/js/services/ConfigurationService.js
@@ -1,4 +1,4 @@
-angular.module('zmon2App').service('ConfigService', ['BootConfig', function (BootConfig) {
+angular.module('zmon2App').service('ConfigurationService', ['BootConfig', function (BootConfig) {
   return {
     getMinCheckInterval: checkId => {
       if (BootConfig.check.minInterval.whitelistedChecks.indexOf(parseInt(checkId)) >= 0) {
