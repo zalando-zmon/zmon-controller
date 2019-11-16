78819577a52b5a0afaf62a8e096fc9d9b02dafbf
Alex Korotkykh <me@alexkorotkikh.com>
2019-11-16 12:13:45 +0100

    support new SDL with sampled ingestion

--- zmon-common/src/main/java/org/zalando/zmon/domain/ServiceLevelStatus.java
+++ zmon-common/src/main/java/org/zalando/zmon/domain/ServiceLevelStatus.java
@@ -1,7 +1,5 @@
 package org.zalando.zmon.domain;
 
-import java.util.HashMap;
-
 public class ServiceLevelStatus {
     private ServiceLevelStatusData data;
     private String lastModified;
@@ -36,23 +34,19 @@ public class ServiceLevelStatus {
 
 
     public static class ServiceLevelStatusData {
-        public static final String FIRST_LEVEL_WARNING = "Metrics visualization is currently limited to the last 12 hours.";
-        public static final String SECOND_LEVEL_WARNING = "Metrics visualization is currently only enabled for metrics classified as \"important\" or \"critical\" and is temporarily limited to the last 12 hours.";
-        public static final String THIRD_LEVEL_WARNING = "Metrics visualization & storage is currently only available for metrics classified as \"important\" or \"critical\" and is temporarily limited to the last 12 hours.";
-        public static final String FOURTH_LEVEL_WARNING = "Storage of metrics is currently only available for metrics classified as \"important\" or \"critical\". Metrics visualization is currently only enabled for metrics classified as \"critical\" and is temporarily limited to the last 12 hours.";
-        public static final String FIFTH_LEVEL_WARNING = "Metrics visualization & storage is currently only available for metrics classified as \"critical\" and is temporarily limited to the last 12 hours.";
+        static final String FIRST_LEVEL_WARNING = "Metrics visualization is currently limited to the last 12 hours.";
+        static final String SECOND_LEVEL_WARNING = "Metrics visualization is currently only enabled for metrics classified as \"important\" or \"critical\" and is temporarily limited to the last 12 hours.";
+        static final String THIRD_LEVEL_WARNING = "Metrics storage for metrics classified as \"other\" is currently sampled by 50%. Metrics visualization is currently only enabled for metrics classified as \"important\" or \"critical\" and is temporarily limited to the last 12 hours.";
+        static final String FOURTH_LEVEL_WARNING = "Metrics visualization & storage is currently only available for metrics classified as \"important\" or \"critical\" and is temporarily limited to the last 12 hours.";
+        static final String FIFTH_LEVEL_WARNING = "Storage of metrics is currently only available for metrics classified as \"important\" or \"critical\". Metrics visualization is currently only enabled for metrics classified as \"critical\" and is temporarily limited to the last 12 hours.";
+        static final String SIXTH_LEVEL_WARNING = "Metrics visualization & storage is currently only available for metrics classified as \"critical\" and is temporarily limited to the last 12 hours.";
 
         private Integer ingestMaxCheckTier = 3;
         private Integer queryDistanceHoursLimit = 0;
         private Integer queryMaxCheckTier = 3;
-        private final HashMap<Integer, String> checkTiers;
+        private Integer sampledCheckTier = 0;
+        private Double sampledCheckRate = 0d;
         private String message;
-
-        public ServiceLevelStatusData() {
-            this.checkTiers = new HashMap<>();
-            this.checkTiers.put(2, "metrics classified as \"important\" and \"critical\"");
-            this.checkTiers.put(1, "metrics classified as \"critical\"");
-        }
 
         public String getMessage() {
             return message;
@@ -69,20 +63,24 @@ public class ServiceLevelStatus {
                 this.message += FIRST_LEVEL_WARNING;
             }
 
-            if (this.queryDistanceHoursLimit == 12 && this.ingestMaxCheckTier == 3 && this.queryMaxCheckTier == 2) {
+            if (this.queryDistanceHoursLimit == 12 && this.ingestMaxCheckTier == 3 && this.queryMaxCheckTier == 2 && this.sampledCheckTier == 0) {
                 this.message += SECOND_LEVEL_WARNING;
             }
 
-            if (this.queryDistanceHoursLimit == 12 && this.ingestMaxCheckTier == 2 && this.queryMaxCheckTier == 2) {
+            if (this.queryDistanceHoursLimit == 12 && this.ingestMaxCheckTier == 3 && this.queryMaxCheckTier == 2 && this.sampledCheckTier == 3) {
                 this.message += THIRD_LEVEL_WARNING;
             }
 
-            if (this.queryDistanceHoursLimit == 12 && this.ingestMaxCheckTier == 2 && this.queryMaxCheckTier == 1) {
+            if (this.queryDistanceHoursLimit == 12 && this.ingestMaxCheckTier == 2 && this.queryMaxCheckTier == 2) {
                 this.message += FOURTH_LEVEL_WARNING;
             }
 
-            if (this.queryDistanceHoursLimit == 12 && this.ingestMaxCheckTier == 1 && this.queryMaxCheckTier == 1) {
+            if (this.queryDistanceHoursLimit == 12 && this.ingestMaxCheckTier == 2 && this.queryMaxCheckTier == 1) {
                 this.message += FIFTH_LEVEL_WARNING;
+            }
+
+            if (this.queryDistanceHoursLimit == 12 && this.ingestMaxCheckTier == 1 && this.queryMaxCheckTier == 1) {
+                this.message += SIXTH_LEVEL_WARNING;
             }
         }
 
@@ -110,16 +108,30 @@ public class ServiceLevelStatus {
             this.queryMaxCheckTier = queryMaxCheckTier;
         }
 
+        public Integer getSampledCheckTier() {
+            return sampledCheckTier;
+        }
+
+        public void setSampledCheckTier(Integer sampledCheckTier) {
+            this.sampledCheckTier = sampledCheckTier;
+        }
+
+        public Double getSampledCheckRate() {
+            return sampledCheckRate;
+        }
+
+        public void setSampledCheckRate(Double sampledCheckRate) {
+            this.sampledCheckRate = sampledCheckRate;
+        }
 
         @Override
         public String toString() {
-            final StringBuilder sb = new StringBuilder("ServiceLevelStatus{");
-            sb.append("ingestMaxCheckTier=").append(ingestMaxCheckTier);
-            sb.append(", queryDistanceHoursLimit=").append(queryDistanceHoursLimit);
-            sb.append(", queryMaxCheckTier=").append(queryMaxCheckTier);
-            sb.append(", message=").append(message);
-            sb.append('}');
-            return sb.toString();
+            return "ServiceLevelStatus{" +
+                    "ingestMaxCheckTier=" + ingestMaxCheckTier +
+                    ", queryDistanceHoursLimit=" + queryDistanceHoursLimit +
+                    ", queryMaxCheckTier=" + queryMaxCheckTier +
+                    ", message=" + message +
+                    '}';
         }
     }
 }
